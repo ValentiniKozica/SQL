a44f96457a5468b3c2640c6369dda3eade187024
Valentini Kožica <valentini.kozica@gmail.com>
2022-03-25 15:30:44 +0100

    Create SetOpenRowset.sql

---
+++ SetOpenRowset.sql
@@ -0,0 +1,13 @@
+--Transact-SQL OPENROWSET
+
+sp_configure 'show advanced options',1
+go
+reconfigure
+go
+sp_configure 'Ad Hoc Distributed Queries',1
+go
+reconfigure
+go
+sp_configure 'xp_cmdshell',1
+go
+reconfigure
